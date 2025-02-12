bffe703e39ae6cbb3e8998b57d1bc31aeabeddea
Maxim Huzmiev <max@huzm.ru>
2025-02-12 19:19:33 +0000

    Several constructors added for Point

--- main.py
+++ main.py
@@ -2,5 +2,14 @@ from point import Point
 
 # START #
 
-p = Point(20, 35)
-p.show()
+p1 = Point()
+p2 = Point(10, 20)
+p3 = Point.empty()
+p4 = Point.from_xy(100, 200)
+p5 = Point.from_copy(p4)
+
+p1.show()
+p2.show()
+p3.show()
+p4.show()
+p5.show()
--- point.py
+++ point.py
@@ -1,13 +1,29 @@
 """ Point module """
+from typing import Self
 
 class Point:
 
-    # Constructor
-    def __init__(self, new_x: int, new_y: int):
-        self._x = new_x
-        self._y = new_y
+    # Constructor with default values
+    def __init__(self, new_x: int = 0, new_y: int = 0):
+        self.__x = new_x
+        self.__y = new_y
+
+    # Pseudo default constructor
+    def empty() -> Self:
+        return Point(0, 0)
+    
+    # Pseudo XY constructor
+    def from_xy(new_x: int, new_y: int):
+        return Point(new_x, new_y)
+
+    # Pseudo copy constructor
+    def from_copy(p: Self) -> Self:
+        temp = Point()
+        temp.__x = p.__x
+        temp.__y = p.__y
+        return temp
 
     # Print point on console
     def show(self) -> None:
-        print(f"X = {self._x}; Y = {self._y}")
+        print(f"X = {self.__x}; Y = {self.__y}")
 
